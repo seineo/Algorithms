d616fb34de8c7a21584545e97d84085d2aecb4d1
seineo <1013248644qq@gmail.com>
2020-09-29 18:38:38 +0800

    get the bonus of the assignment of week2
    use reservoir sampling

--- Part1/week2/Permutation.java
+++ Part1/week2/Permutation.java
@@ -6,16 +6,32 @@
 
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
 
 public class Permutation {
     public static void main(String[] args) {
         RandomizedQueue<String> queue = new RandomizedQueue<>();
         int k = Integer.parseInt(args[0]);
-        while (!StdIn.isEmpty()) {
+        int i = 1;
+        // use reservoir sampling
+        // 1. enqueue the first k elements
+        for (int j = 0; j < k ; j++) {
             queue.enqueue(StdIn.readString());
         }
-        while (k-- > 0) {
+        // 2. for the nth element, it has the possibility of k/n to replace
+        //    one of the element in queue
+        while (!StdIn.isEmpty()) {
+            String str = StdIn.readString();
+            int random = StdRandom.uniform(0, k + i);
+            if (random < k) {
+                queue.dequeue();
+                queue.enqueue(str);
+            }
+            i++;
+        }
+        while (!queue.isEmpty()) {
             StdOut.println(queue.dequeue());
         }
     }
 }
+
